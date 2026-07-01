fdd3bd5ae46bb3e3ea5980a65d9cc0a07b812f90
Kaushalya04 <GAHDSE252F-012@student.nibm.lk>
2026-07-01 11:31:52 +0530

    Created register screen UI

--- app/src/main/java/com/nibm/moneymate/auth/RegisterActivity.java
+++ app/src/main/java/com/nibm/moneymate/auth/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.nibm.moneymate.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +10,19 @@ import com.nibm.moneymate.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView txtLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txtLogin = findViewById(R.id.txtLogin);
+
+        txtLogin.setOnClickListener(
+                v -> {
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+        });
     }
 }
