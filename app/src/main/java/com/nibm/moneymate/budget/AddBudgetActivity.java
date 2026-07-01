f36220432834a2a53771e149c923a1d0c7b2a0f4
Kaushalya04 <GAHDSE252F-012@student.nibm.lk>
2026-07-01 22:03:20 +0530

    Update

--- app/src/main/java/com/nibm/moneymate/budget/AddBudgetActivity.java
+++ app/src/main/java/com/nibm/moneymate/budget/AddBudgetActivity.java
@@ -2,11 +2,7 @@ package com.nibm.moneymate.budget;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.nibm.moneymate.R;
 
@@ -15,9 +11,6 @@ public class AddBudgetActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_budget);
-
-
     }
 }
